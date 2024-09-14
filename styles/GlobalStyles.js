354ea75e66ee761cbac885956703bbd2b7cb5c02
TitanService <126705709+Paul1502@users.noreply.github.com>
2024-09-14 20:34:45 +0200

    Update GlobalStyles.js

--- styles/GlobalStyles.js
+++ styles/GlobalStyles.js
@@ -1,14 +1,23 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  body {
+  * {
     margin: 0;
-    background-color: #000;
-    color: #fff;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: #000000;
+    color: #FFFFFF;
     font-family: 'Roboto', sans-serif;
     overflow-x: hidden;
   }
-  /* Weitere globale Stile */
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
 `;
 
 export default GlobalStyle;
