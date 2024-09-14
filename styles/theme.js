7340a3137f26cc177e7f7d466e09f23437f7f47e
TitanService <126705709+Paul1502@users.noreply.github.com>
2024-09-14 20:31:25 +0200

    Update theme.js

---
+++ styles/theme.js
@@ -0,0 +1,13 @@
+const theme = {
+  colors: {
+    primary: '#00FFFF', // Neonblau
+    secondary: '#FF00FF', // Neonpink
+    accent: '#00FF00', // Neongrün
+  },
+  fonts: {
+    main: "'Roboto', sans-serif",
+    heading: "'Orbitron', sans-serif",
+  },
+};
+
+export default theme;
